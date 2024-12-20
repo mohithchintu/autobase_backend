a2ad0d2334f607f912bf2433c7e9b788939e0b47
mohithchintu <konireddynagamohithreddy@gmail.com>
2024-12-20 23:11:44 +0530

    CORS added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import connectDb from "./config/connectDB.js";
 import router from "./routes/routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 connectDb();
 
-const PORT = process.env.PORT || 5000;
+const allowedOrigins = ["http://localhost:5174"];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(router);
 
@@ -16,6 +27,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
